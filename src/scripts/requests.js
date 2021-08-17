47e198fc4e8afb0de8de97c6ab818c0f77b67610
Matthew Singler <matthew.singler@gmail.com>
2021-08-17 13:38:37 -0500

    fixed 404 errors with Deny Request button. Had everything nested inside the request function so it was rendering twice.

--- src/scripts/requests.js
+++ src/scripts/requests.js
@@ -1,31 +1,31 @@
 import { getRequests } from "./dataAccess.js";
 import { deleteRequest } from "./dataAccess.js";
+
+
+const allEventRequestsInListForm = (request) => {
+    return `<li>${request.childName}<button class="request__delete" id="request--${request.id}">
+    DENY
+    </button>
+    </li>`
+}
+
 export const Requests = () => {
 
     const requests = getRequests()
 
-    const allEventRequestsInListForm = (request) => {
-        return `<li>${request.childName}<button class="request__delete" id="request--${request.id}">
-        DENY
-        </button>
-        </li>`
-    }
 
     let html = `
         <ul>
             ${requests.map(allEventRequestsInListForm).join("")}
         </ul>
     `
-
-    const mainContainer = document.querySelector("#container")
-
-    mainContainer.addEventListener("click", click => {
-        if (click.target.id.startsWith("request--")) {
-            const [, requestId] = click.target.id.split("--")
-            deleteRequest(parseInt(requestId))
-        }
-    })
-
-
     return html
 }
+const mainContainer = document.querySelector("#container")
+
+mainContainer.addEventListener("click", click => {
+    if (click.target.id.startsWith("request--")) {
+        const [, requestId] = click.target.id.split("--")
+        deleteRequest(parseInt(requestId))
+    }
+})
